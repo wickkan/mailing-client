44bb815be1df3423328a86d53ddd9f54dc96903f
Kanishka <kanishkawickramasinghe50@gmail.com>
2022-02-19 12:12:51 +1100

    more code

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import smtplib
+
+server = smtplib.SMTP('smtp.gmail.com', 25)
+server.ehlo()
+
+with open('password.txt', 'r') as f:
+    password = f.read()
+
+server.login('throwaway197256@gmail.com', password)
