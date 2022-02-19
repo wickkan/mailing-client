536fe96d532924181686a9450ffa78a2b4de67cc
Kanishka <kanishkawickramasinghe50@gmail.com>
2022-02-19 12:19:08 +1100

    added msg.txt

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import smtplib
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+
 
 server = smtplib.SMTP('smtp.gmail.com', 25)
 server.ehlo()
@@ -7,3 +12,13 @@ with open('password.txt', 'r') as f:
     password = f.read()
 
 server.login('throwaway197256@gmail.com', password)
+
+msg = MIMEMultipart()
+msg['From'] = 'John'
+msg['To'] = 'testmails@spam1.de'
+msg['Subject'] = 'Test Project'
+
+with open('msg.txt', 'r') as f:
+    msg = f.read()
+
+msg.attach()
